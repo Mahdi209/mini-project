e680ef960ab7aa19bb5d83b2e6d761cb63545156
Mohammed Mahdi <155379064+Mahdi209@users.noreply.github.com>
2024-02-19 00:10:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+function inputUser() {
+  const user_input = prompt("Enter Rock, Paper, Scissors").toLowerCase();
+
+  return user_input;
+}
+
+function randomOptionForComputer() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  const chocsComputer = choices[randomNumber];
+  return chocsComputer;
+}
+
+function playGame() {
+  let playerCount = 0;
+  let computeCount = 0;
+  for (let i = 1; i <= 5; i++) {
+    let player = inputUser();
+    let computer = randomOptionForComputer();
+    console.log(computer);
+    if (
+      (player === "scissors" && computer === "rock") ||
+      (player === "rock" && computer === "paper") ||
+      (player === "paper" && computer === "scissors")
+    ) {
+      alert(`round ${i} to lose`);
+      computeCount++;
+    } else if (
+      (player === "paper" && computer === "rock") ||
+      (player === "rock" && computer === "scissors") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      alert(`round ${i} to won`);
+      playerCount++;
+    } else {
+      alert(`round ${i} drawn `);
+    }
+  }
+
+  if (playerCount > computeCount) {
+    console.log(
+      `Congratulations! You emerged victorious, winning ${playerCount} rounds out of 5.`
+    );
+  } else if (playerCount < computeCount) {
+    console.log(
+      `Oops! The computer dominated, winning ${computeCount} rounds out of 5.`
+    );
+  } else {
+    console.log("It's a close match! The player and the computer are tied.");
+  }
+}
+
+playGame();
